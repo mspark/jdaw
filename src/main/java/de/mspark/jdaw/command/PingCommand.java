cca21a7a5f3609f4673238569f65fcae5f83b526
mspark <marcel-maze7@hotmail.de>
2021-12-02 23:14:56 +0100

    New Ping Command

---
+++ src/main/java/de/mspark/jdaw/command/PingCommand.java
@@ -0,0 +1,27 @@
+package de.mspark.jdaw.command;
+
+import java.util.List;
+
+import de.mspark.jdaw.jda.JDAManager;
+import de.mspark.jdaw.jda.JDAWConfig;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+@CommandProperties(trigger = "ping", description = "Replies with pong", helpPage = false)
+public class PingCommand extends Command {
+
+    public PingCommand(JDAWConfig conf, JDAManager jdas) {
+        super(conf, jdas);
+    }
+
+    @Override
+    public void doActionOnCmd(Message msg, List<String> cmdArguments) {
+        msg.reply("pong");
+    }
+
+    @Override
+    protected MessageEmbed fullHelpPage() {
+        return null;
+    }
+
+}
