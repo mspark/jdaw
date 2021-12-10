0f3db26620681efb6fd343a5c5efb186b069fc8e
mspark <marcel-maze7@hotmail.de>
2021-12-10 22:49:24 +0100

    ft: command for changing the prefix

---
+++ src/main/java/de/mspark/jdaw/guilds/NoActionRepository.java
@@ -0,0 +1,40 @@
+package de.mspark.jdaw.guilds;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import de.mspark.jdaw.guilds.model.CustomGuildConf;
+
+@Configuration
+class NoActionRepository {
+
+    @Bean
+    @Lazy
+    public GuildRepository guildRepository() {
+        return new GuildRepository() {
+            
+            @Override
+            public Optional<CustomGuildConf> findOne(Long gid) {
+                return Optional.empty();
+            }
+            
+            @Override
+            public boolean exists(Long gid) {
+                return false;
+            }
+            
+            @Override
+            public void delete(CustomGuildConf entity) {
+                // nothing
+            }
+
+            @Override
+            public CustomGuildConf save(CustomGuildConf g) {
+                return null;
+            }
+        };
+    }
+}
