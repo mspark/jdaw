a939e7210487beecf06016be952dee518bf32b59
mspark <marcel-maze7@hotmail.de>
2022-03-13 19:26:31 +0100

    ch: exception message for missing configuration

--- src/main/java/de/mspark/jdaw/config/JDAConfiguration.java
+++ src/main/java/de/mspark/jdaw/config/JDAConfiguration.java
@@ -17,9 +17,14 @@ public class JDAConfiguration {
 
     @Bean
     public JDA[] jdas(JDAWConfig conf, List<JDAConfigurationVisitor> jdaVisitor) throws LoginException {
+        if (conf.apiTokens() == null || conf.apiTokens().length == 0) {
+            throw new RuntimeException("No Discord API-Tokens found: Bean is present but has no values");
+        }
         List<JDABuilder> jdaBuilderList = Stream.of(conf.apiTokens()).map(JDABuilder::createDefault).toList();
         jdaBuilderList.forEach(a -> jdaVisitor.forEach(j -> j.visit(a)));
         return jdaBuilderList.stream().map(Unchecked.function(JDABuilder::build)).toArray(JDA[]::new);
     }
 
 }
+
+
