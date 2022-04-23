671b8bcb89bc038a0776926637a33a54f1243289
mspark <marcel-maze7@hotmail.de>
2022-04-23 12:45:54 +0200

    rf: simplify the global help command setup

--- src/main/java/de/mspark/jdaw/help/GlobalHelpCommand.java
+++ src/main/java/de/mspark/jdaw/help/GlobalHelpCommand.java
@@ -1,34 +1,43 @@
 package de.mspark.jdaw.help;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 
 import de.mspark.jdaw.Command;
-import de.mspark.jdaw.JDAManager;
+import de.mspark.jdaw.CommandProperties;
 import de.mspark.jdaw.DistributionSetting;
-import de.mspark.jdaw.config.JDAWConfig;
+import de.mspark.jdaw.JDAManager;
 import de.mspark.jdaw.guilds.GuildConfigService;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
 /**
- * When implementing this with the {@link EnableHelpCommand} Annotation, a help command for all sub commands will be available .
+ * When implementing this with the {@link EnableHelpCommand} Annotation, a help command for all sub commands will be
+ * available.
  *
  * @author marcel
  */
-public abstract class GlobalHelpCommand extends Command {
-    
-    protected List<Command> allLoadedCmds; 
-    
-    public GlobalHelpCommand(GuildConfigService gc, JDAManager jdas, List<Command> allLoadedCmds) {
+public class GlobalHelpCommand extends Command {
+
+    @CommandProperties(trigger = "sample", description = "sample")
+    static class DefaultProperties {
+    }
+
+    private HelpConfig config;
+    private List<Command> allLoadedCmds;
+
+    public GlobalHelpCommand(GuildConfigService gc, JDAManager jdas, List<Command> allLoadedCmds, HelpConfig config) {
         super(gc, jdas, DistributionSetting.MAIN_ONLY);
         this.allLoadedCmds = allLoadedCmds;
+        this.config = config;
     }
-    
+
     @Override
     public void doActionOnCmd(Message msg, List<String> cmdArguments) {
         if (cmdArguments.isEmpty()) {
-            var eb = new EmbedBuilder().setTitle(botName()).setDescription(botDescription());
+            var eb = new EmbedBuilder().setTitle(config.botName()).setDescription(config.botDescription());
             allLoadedCmds.stream()
                 .filter(cmd -> cmd.userHasEnoughPermission(msg))
                 .filter(cmd -> cmd.helpPage(msg).isPresent())
@@ -42,29 +51,90 @@ public abstract class GlobalHelpCommand extends Command {
                 .filter(cmd -> cmd.userHasEnoughPermission(msg))
                 .flatMap(c -> c.helpPage(msg))
                 .ifPresentOrElse(
-                        helpPage -> msg.getChannel().sendMessage(helpPage).submit(), 
-                        () -> msg.reply("No help page").submit()
-                );
+                    helpPage -> msg.getChannel().sendMessage(helpPage).submit(),
+                    () -> msg.reply("No help page").submit());
         }
     }
-    
+
     @Override
     public MessageEmbed fullHelpPage() {
         throw new UnsupportedOperationException();
     }
-    
-    public abstract String botDescription();
-    
-    public abstract String botName();
 
     @Override
     public String getShortDescription() {
         throw new UnsupportedOperationException();
     }
-    
+
     @Override
-    public final String getTrigger() {
+    public String getTrigger() {
         return "help";
     }
-    
+
+    /*
+     * This class has no @CommandProperties annotation because the bean is created elsewhere. Through that, we need to
+     * provide an instance of the annotation manually.
+     */
+    @Override
+    public CommandProperties commandProperties() {
+        var props = DefaultProperties.class.getAnnotation(CommandProperties.class);
+        var command = this;
+        return new CommandProperties() {
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return props.annotationType();
+            }
+
+            @Override
+            public Permission[] userGuildPermissions() {
+                return props.userGuildPermissions();
+            }
+
+            @Override
+            public Permission[] userChannelPermissions() {
+                return props.userChannelPermissions();
+            }
+
+            @Override
+            public String trigger() {
+                return command.getTrigger();
+            }
+
+            @Override
+            public boolean helpPage() {
+                return props.helpPage();
+            }
+
+            @Override
+            public boolean executableWihtoutArgs() {
+                return true;
+            }
+
+            @Override
+            public String description() {
+                return command.getShortDescription();
+            }
+
+            @Override
+            public Permission[] botGuildPermissions() {
+                return props.botGuildPermissions();
+            }
+
+            @Override
+            public boolean botAdminOnly() {
+                return props.botAdminOnly();
+            }
+
+            @Override
+            public String[] aliases() {
+                return props.aliases();
+            }
+
+            @Override
+            public boolean privateChatAllowed() {
+                return props.privateChatAllowed();
+            }
+        };
+    }
 }
